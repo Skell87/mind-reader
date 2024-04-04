93d24976dfa934d6af22f9fa510f735136045d6c
Brandon <brandon.heisler@gmail.com>
2024-04-04 12:00:16 -0400

    created buttons

--- main.js
+++ main.js
@@ -5,3 +5,43 @@ function init(){
   }
   
   init()
+
+
+// make states to switch between.
+
+const state1 = {
+  textbox : "I can read your mind.",
+}
+
+const state2 = {
+  textbox : "Pick a number from 01-99",
+}
+
+const state3 = {
+  textbox : "Add both digits together to get a new number.",
+}
+
+// function stateUpdater(){
+// if next button is clicked, id="topTextBox changes from state2 to state 3"
+  // if(lowerButton)
+// }
+
+
+
+
+// BUTTONS:
+// this is the NEXT button.
+function onNextClick(){
+  alert("Next button clicked");
+}
+
+const nextButton = document.getElementById('nextButton');
+nextButton.addEventListener('click', onNextClick);
+
+// this is the GO/RESET button.
+function onGoResetClick(){
+  alert("GoReset button clicked");
+}
+
+const lowerButton = document.getElementById('goReturnButton');
+lowerButton.addEventListener('click', onGoResetClick);
