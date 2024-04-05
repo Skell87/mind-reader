a630b29aab656461888e294f781278e6f741e83d
Brandon <brandon.heisler@gmail.com>
2024-04-05 09:24:40 -0400

    list made, pre positioning

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function init(){
   }
   
   init()
-
+  
 
 
 let currentState = 0
@@ -22,31 +22,76 @@ function drawScreen(){
     document.getElementById('topTextBox').textContent="I can read your mind...";
     document.getElementById('nextButton').style.visibility="hidden";
     document.getElementById('bottomTextBox').textContent="";
+    document.getElementById('goButton').textContent="go";
+    
   }
   else if (currentState === 1){
     document.getElementById('topTextBox').textContent="Pick a number from 01 - 99";
     document.getElementById('nextButton').style.visibility="visible";
-    document.getElementById('bottomTextBox').textContent="when you have your number click next"
+    document.getElementById('bottomTextBox').textContent="when you have your number click next";
+    document.getElementById('goButton').textContent="return";
+    
+    
   }
   else if(currentState === 2){
     document.getElementById('topTextBox').textContent="Add both digits together to get a new number";
     document.getElementById('nextButton').style.visibility="visible";
-    document.getElementById('bottomTextBox').textContent="Ex: 13 is 1 + 4 = 5, click next to proceed"
+    document.getElementById('bottomTextBox').textContent="Ex: 13 is 1 + 4 = 5, click next to proceed";
+    document.getElementById('goButton').textContent="return";
+    
   }
   else if(currentState === 3){
     document.getElementById('topTextBox').textContent="Subtract your new number from the original number";
     document.getElementById('nextButton').style.visibility="visible";
-    document.getElementById('bottomTextBox').textContent="Ex: 14 - 5 = 9, click next to proceed"
+    document.getElementById('bottomTextBox').textContent="Ex: 14 - 5 = 9, click next to proceed";
+    document.getElementById('goButton').textContent="return";
+    
   }
   else if(currentState === 4){
     document.getElementById('topTextBox').textContent="";
     document.getElementById('nextButton').style.visibility="visible";
-    document.getElementById('bottomTextBox').textContent="find your new number. Note the symbol beside the number"
+    document.getElementById('bottomTextBox').textContent="find your new number. Note the symbol beside the number";
+    document.getElementById('goButton').textContent="return";
+
+    
+    let list = document.createElement("ul");
+    list.classList.add("listFunc");
+    
+
+    let ttb = document.getElementById('appBody');
+    
+    function addItems(character, parent){
+      let li = document.createElement("li")
+      li.textContent = character;
+      parent.appendChild(li);
+    }
+
+
+    for (i = 0; i < 11; i++){
+      addItems("!", list)
+      addItems("@", list)
+      addItems("#", list)
+      addItems("$", list)
+      addItems("%", list)
+      addItems("^", list)
+      addItems("&", list)
+      addItems("*", list)
+      addItems("?", list)
+    }
+    console.log(list)
+    ttb.appendChild(list)
+
+
+
+    
+  
   }
   else if(currentState === 5){
     document.getElementById('topTextBox').textContent="REVEAL THE CORRECT PIC";
     document.getElementById('nextButton').style.visibility="hidden";
-    document.getElementById('bottomTextBox').textContent="Your symbol is: "
+    document.getElementById('bottomTextBox').textContent="Your symbol is: ";
+    document.getElementById('goButton').textContent="return";
+    
   }
 
 }
@@ -69,6 +114,7 @@ function onGoClick(){
   // alert("GoReset button clicked");
   if (currentState == 0){
   currentState++;
+  
   drawScreen();
 }
   else {
@@ -81,3 +127,4 @@ const lowerButton = document.getElementById('goButton');
 lowerButton.addEventListener('click', onGoClick);
 
 // this is the reset button.
+
